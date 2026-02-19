dbcf2f45d552973eaed9dd186c5c8cd9c7d9afcb
Ines Tapia <inestapia2608@gmail.com>
2026-02-18 21:23:41 -0600

    Declaracion constructor y destructor

---
+++ Matriz.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <filestream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Matriz(){
+	private:
+		numFilas;
+		numColumnas;
+	
+	public:
+		Matriz(const std:: string &ruta);
+		~Matriz();
+		
+		
+		
+		
+	
+}
